950866c12eeb3f722d991524181b1024dac7671c
jurob <jurobega2@gmail.com>
2024-11-28 19:01:44 +0100

    clase ControladorRobot terminado

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
@@ -0,0 +1,22 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public class ControladorRobot {
+    private Robot robot;
+
+    public ControladorRobot( Robot robot )  {
+        this.robot = new Robot(robot);
+    }
+
+    public Robot getRobot() {
+        return new Robot();
+    }
+
+    public void ejecutar( char comando ) throws RobotExcepcion {
+        switch (comando) {
+            case 'A', 'a' -> robot.avanzar();
+            case 'D', 'd' -> robot.girarALaDerecha();
+            case 'I', 'i' -> robot.girarALaIzquierda();
+            default -> throw new RobotExcepcion("Comando desconocido.");
+        }
+    }
+}
